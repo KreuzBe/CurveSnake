0902d40aab545ea1f7c267e828b9219b4bc22477
KreuzBe <KreuzBe@web.de>
2021-01-23 21:53:04 +0100

    Small CHanges

--- Main.java
+++ Main.java
@@ -18,7 +18,7 @@ public class Main extends Display {
         ImageLoader.init();
         if (isServer()) {
             System.out.println("I AM A PLAYER!!!!!!!!!!!!!!");
-            addMoveable(new Player(50, 50, 1, 1, 3, this, BYTE_PLAYER_MIN));
+            addMoveable(new Player(50, 50, 1, 1, 0, this, BYTE_PLAYER_MIN));
         } else {
             addMoveable(new Player(500, 500, -1, -1, 3, this, BYTE_PLAYER_MIN << 1));
         }
