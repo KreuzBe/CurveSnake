837c110dc4e9f1a130dca09707cd23576fa70dba
KreuzBe <kreuzbe@web.de>
2021-01-25 00:33:55 +0100

    Debugging

--- Main.java
+++ Main.java
@@ -1,19 +1,20 @@
 import visual.Display;
 import visual.ImageLoader;
+import visual.Moveable;
 import visual.PowerUp;
 import visual.moveable.Enemy;
 import visual.moveable.Player;
 
 public class Main extends Display {
 
-    private Player player;
+    private Moveable player;
     private Enemy enemy;
 
     public Main() {
         super(true);
         ImageLoader.init();
         if (isServer())
-            player = new Player(50, 50, 1, 1, 1, this, BYTE_PLAYER_MIN);
+            player = new Enemy(50, 50, 1, 1, 3, this, null);
         else
             player = new Player(500, 500, -1, 1, 5, this, BYTE_PLAYER_MIN << 1);
         addMoveable(player);
