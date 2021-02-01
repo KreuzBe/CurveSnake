f79c54211ab4333e2ef4d231cdb31586f33b5116
KreuzBe <kreuzbe@web.de>
2021-02-01 12:42:17 +0100

    debugging

--- visual/Moveable.java
+++ visual/Moveable.java
@@ -173,7 +173,7 @@ public class Moveable extends VisualObject implements Serializable {
 
 
     public void onCrash(int code) {
-        if (isRemoteControlled()) return;
+        // if (isRemoteControlled()) return;
         System.out.print(this + " was killed by ");
         if ((code & Display.BYTE_PLAYER) != 0) {
             System.out.println("player " + ((int) (Math.log(code & Display.BYTE_PLAYER) / Math.log(2)) - Display.BYTE_SHIFT_PLAYER + 1));
